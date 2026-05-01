5bf36a2252c4e1a955f1479e757b7753b0e0dfdd
Chini-2903 <chinmaya0810@gmail.com>
2026-05-01 10:52:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-﻿const express = require('express');
+const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
-
+const io = new Server(server, { 
+    cors: { 
+        origin: ["https://phaser-multiplayer-arena.vercel.app", "http://localhost:5173"], 
+        methods: ["GET", "POST"],
+        credentials: true
+    } 
+});
 const rooms = {}; 
 const playerMappings = {}; 
 
